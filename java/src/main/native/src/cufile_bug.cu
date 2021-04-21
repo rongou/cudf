656b56ab4c582fa705523bf1c709cc5103b1cf51
Rong Ou <rong.ou@gmail.com>
2021-04-21 12:39:21 -0700

    use threads

--- java/src/main/native/src/cufile_bug.cu
+++ java/src/main/native/src/cufile_bug.cu
@@ -1,11 +1,12 @@
 // build:
-// nvcc -lcufile cufile_bug.cu
+// nvcc --default-stream per-thread -lcufile cufile_bug.cu
 //
 // run:
 // ./a.out
 
 #include <cassert>
 #include <string>
+#include <thread>
 
 #include <cufile.h>
 #include <fcntl.h>
@@ -13,36 +14,41 @@
 
 #include <sys/stat.h>
 
-int main() {
-  auto const size = 1 << 30;
+#define DEVICE 0
+#define SIZE 1 << 30
+#define BASE "/tmp/cufile"
 
-  assert(cudaSetDevice(1) == cudaSuccess);
-  assert(cudaFree(0) == cudaSuccess);
-
-  void *buffers[2];
+void write_buffers(void **buffers) {
   for (int i = 0; i < 2; i++) {
-    assert(cudaMalloc(&buffers[i], size) == cudaSuccess);
-    assert(cudaMemset(buffers[i], i, size) == cudaSuccess);
-  }
-
-  assert(cuFileDriverOpen().err == CU_FILE_SUCCESS);
-
-  for (int i = 0; i < 2; i++) {
-    assert(cudaSetDevice(1) == cudaSuccess);
-    auto const file_descriptor = open(("/data/rou/tmp/cufile" + std::to_string(i)).c_str(),
+    assert(cudaSetDevice(DEVICE) == cudaSuccess);
+    auto const file_descriptor = open((BASE + std::to_string(i)).c_str(),
                                       O_CREAT | O_WRONLY | O_DIRECT, S_IRUSR | S_IWUSR);
     assert(file_descriptor >= 0);
     CUfileDescr_t cufile_descriptor{CU_FILE_HANDLE_TYPE_OPAQUE_FD, file_descriptor};
     CUfileHandle_t cufile_handle{};
     assert(cuFileHandleRegister(&cufile_handle, &cufile_descriptor).err == CU_FILE_SUCCESS);
 
-    assert(cuFileWrite(cufile_handle, buffers[i], size, 0, 0) == size);
+    assert(cuFileWrite(cufile_handle, buffers[i], SIZE, 0, 0) == SIZE);
 
     assert(cudaFree(buffers[i]) == cudaSuccess);
     cuFileHandleDeregister(cufile_handle);
     assert(close(file_descriptor) == 0);
   }
+}
 
+int main() {
+  assert(cudaSetDevice(DEVICE) == cudaSuccess);
+  assert(cudaFree(0) == cudaSuccess);
+
+  void *buffers[2];
+  for (int i = 0; i < 2; i++) {
+    assert(cudaMalloc(&buffers[i], SIZE) == cudaSuccess);
+    assert(cudaMemset(buffers[i], i, SIZE) == cudaSuccess);
+  }
+
+  assert(cuFileDriverOpen().err == CU_FILE_SUCCESS);
+  std::thread t{write_buffers, buffers};
+  t.join();
   assert(cuFileDriverClose().err == CU_FILE_SUCCESS);
 
   return 0;
